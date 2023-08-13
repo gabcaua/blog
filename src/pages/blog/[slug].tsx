b5d3944550b74e44a2266aac64f46862c1e9a92b
CAUÃ GABRIEL DA SILVA <65617660+gabcaua@users.noreply.github.com>
2023-08-13 14:44:38 +0000

    descPre

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -125,7 +125,7 @@ const RenderPost = ({ post, redirect, preview }) => {
   // If the page is not yet generated, this will be displayed
   // initially until getStaticProps() finishes running
   if (router.isFallback) {
-    return <div>Loading...</div>
+    return <div>Carregando...</div>
   }
 
   // if you don't have a post at this point, and are not
